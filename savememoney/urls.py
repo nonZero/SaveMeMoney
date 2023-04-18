1afb6440f525b026102581eb69320d8353064125
Udi Oron <udioron@gmail.com>
2023-04-18 12:36:41 +0300

    some more view simple examples

--- savememoney/urls.py
+++ savememoney/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import random
+
 from django.contrib import admin
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest, JsonResponse, HttpResponseNotAllowed
 from django.urls import path
+
 
 def my_view(request):
     a = 239487324
@@ -24,13 +27,31 @@ def my_view(request):
     c = a * b
     return HttpResponse(f"c is {c}")
 
-def my_age_is(request, age: int):
-    return HttpResponse(f"My age is {age}")
 
+def birthday(request: HttpRequest, name: str, age: int):
+    if name == "shooki":
+        return HttpResponseNotAllowed("no!")
+    return HttpResponse(f"{name.upper()} is {age} years old.")
+
+
+def add_view(request, a: int, b: int):
+    return HttpResponse(f"{a} + {b} = <b>{a + b}</b>")
+
+
+def api_add_view(request, a: int, b: int):
+    return JsonResponse(
+        {
+            "result": a + b,
+            "surprise": random.randint(1, 100),
+        }
+    )
 
 
 urlpatterns = [
     path("", my_view),
-    path("age/<int:age>/", my_age_is),
+    path("age/<name>/<int:age>/", birthday),
+    path("age/<int:age>/<name>/", birthday),
+    path("add/<int:a>/<int:b>/", add_view),
+    path("api/add/<int:a>/<int:b>/", api_add_view),
     path("admin/", admin.site.urls),
 ]
