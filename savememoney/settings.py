1df8617a8781aef186695ec05f6f8099c279861e
Udi Oron <udioron@gmail.com>
2023-06-20 16:19:06 +0300

    move to postgres

--- savememoney/settings.py
+++ savememoney/settings.py
@@ -82,8 +82,8 @@ WSGI_APPLICATION = "savememoney.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "save_me_money",
     }
 }
 
